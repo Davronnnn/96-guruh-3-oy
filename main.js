9576809512d4e5e414a344cb7b3c6de3b6375141
Davronnn <kifleader@gmail.com>
2022-12-13 07:10:11 +0500

    last

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-// var name = prompt('ismingizni kiriting');
+// var money = Number(prompt(`pulingizni kiriting`));
 
-var surName = "bizning \n e'lon ";
+// var dollarToSum = 10650.34;
+// var euroToSum = 10650.03;
 
-console.log(surName);
+// var pricePlane = 500;
+// var priceHotel = 250;
+
+// var priceFun = 120;
+
+// var totalPrice =
+// 	pricePlane * dollarToSum + priceHotel * dollarToSum + priceFun * euroToSum;
+
+// if (money >= totalPrice) {
+// 	var money2 = Math.round(money - totalPrice);
+
+// 	alert(`Mablag'ingiz yetarli, ${money2} so'm pulingiz ortib qoldi`);
+// } else {
+// 	alert('pulingiz yetarli emas');
+// }
+// console.log(Number(true));
+
+console.log(12 - "12");
