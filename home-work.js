173f9fd58427dd7ab93c1f6b7d4ea759500565c3
Davronnn <kifleader@gmail.com>
2022-12-19 21:03:05 +0500

    5-dar

--- home-work.js
+++ home-work.js
@@ -6,6 +6,16 @@ const elInput = document.querySelector('#input');
 const elForm = document.querySelector('#form');
 const elReset = document.querySelector('#resetButton');
 const header = document.querySelector('#header');
+
+function generateTime(space, speed) {
+	const fullTime = space / speed;
+	const hour = Math.floor(fullTime);
+	const minute = Math.floor((fullTime - hour) * 60);
+	const second = Math.floor(((fullTime - hour) * 60 - minute) * 60);
+
+	return `${hour} soat, ${minute} daqiqa, ${second}  soniyada yetib keladi `;
+}
+
 elReset.addEventListener('click', function () {
 	elPerson.textContent = 'N soat';
 	elBicycle.textContent = 'N soat';
@@ -26,38 +36,10 @@ elForm.addEventListener('submit', function (evt) {
 	const planeSpeed = 800;
 
 	if (!isNaN(space)) {
-		const personFullTime = space / personSpeed;
-		const personHour = Math.floor(personFullTime);
-		const personMinute = Math.floor((personFullTime - personHour) * 60);
-		const personSecond = Math.floor(
-			((personFullTime - personHour) * 60 - personMinute) * 60
-		);
-
-		const bicycleFullTime = space / bicycleSpeed;
-		const bicycleHour = Math.floor(bicycleFullTime);
-		const bicycleMinute = Math.floor((bicycleFullTime - bicycleHour) * 60);
-		const bicycleSecond = Math.floor(
-			((bicycleFullTime - bicycleHour) * 60 - bicycleMinute) * 60
-		);
-
-		const carFullTime = space / carSpeed;
-		const carHour = Math.floor(carFullTime);
-		const carMinute = Math.floor((carFullTime - carHour) * 60);
-		const carSecond = Math.floor(
-			((carFullTime - carHour) * 60 - carMinute) * 60
-		);
-
-		const planeFullTime = space / planeSpeed;
-		const planeHour = Math.floor(planeFullTime);
-		const planeMinute = Math.floor((planeFullTime - planeHour) * 60);
-		const planeSecond = Math.floor(
-			((planeFullTime - planeHour) * 60 - planeMinute) * 60
-		);
-
-		elPerson.textContent = `${personHour} soat, ${personMinute} daqiqa, ${personSecond}  soniyada yetib keladi `;
-		elBicycle.textContent = `${bicycleHour} soat, ${bicycleMinute} daqiqa, ${bicycleSecond}  soniyada yetib keladi `;
-		elCar.textContent = `${carHour} soat, ${carMinute} daqiqa, ${carSecond}  soniyada yetib keladi `;
-		elPlane.textContent = `${planeHour} soat, ${planeMinute} daqiqa, ${planeSecond}  soniyada yetib keladi `;
+		elPerson.textContent = generateTime(space, personSpeed);
+		elBicycle.textContent = generateTime(space, bicycleSpeed);
+		elCar.textContent = generateTime(space, carSpeed);
+		elPlane.textContent = generateTime(space, planeSpeed);
 	} else {
 		console.log('raqam kiriting');
 	}
--- main.js
+++ main.js
@@ -1,10 +1,63 @@
-let i = 0;
-// while (i == 10) {
-// 	console.log(i);
+const elInput = document.querySelector('#number-input');
+const elCircle1 = document.querySelector('.circle-1');
+const elCircle2 = document.querySelector('.circle-2');
 
-// 	i++;
-// }
+const elBtn0 = document.querySelector('#btn0');
+const elBtn1 = document.querySelector('#btn1');
+const elBtn2 = document.querySelector('#btn2');
+const elBtn3 = document.querySelector('#btn3');
+const elBtn4 = document.querySelector('#btn4');
+const elBtn5 = document.querySelector('#btn5');
+const elBtn6 = document.querySelector('#btn6');
+const elBtn7 = document.querySelector('#btn7');
+const elBtn8 = document.querySelector('#btn8');
+const elBtn9 = document.querySelector('#btn9');
 
-// for (let index = 0; index < 10; index++) {
+function resetHandler() {
+	elInput.value = '';
+	elCircle1.className = 'circle circle-1';
+	elCircle2.className = 'circle circle-2';
+	elCircle1.textContent = '';
+	elCircle2.textContent = '';
+}
 
-// }
+function checkInput() {
+	if (elInput.value >= 0 && elInput.value < 10) {
+		elCircle1.className += ' circle--active';
+		elCircle1.textContent = 'Active';
+		elCircle2.className = 'circle circle-2';
+		elCircle2.textContent = '';
+	} else if (elInput.value >= 10 && elInput.value < 100) {
+		elCircle2.className += ' circle--active';
+		elCircle2.textContent = 'Active';
+		elCircle1.className = 'circle circle-1';
+		elCircle1.textContent = '';
+	}
+}
+
+function inputHandler(value) {
+	if (
+		elInput.value === '' ||
+		(elInput.value < 10 && elInput.value.length < 2)
+	) {
+		elInput.value += value;
+	}
+}
+
+function eventHandler(elBtn, value) {
+	elBtn.addEventListener('click', function () {
+		inputHandler(value);
+		checkInput();
+	});
+}
+
+eventHandler(elBtn0, '0');
+eventHandler(elBtn1, '1');
+eventHandler(elBtn2, '2');
+eventHandler(elBtn3, '3');
+eventHandler(elBtn4, '4');
+eventHandler(elBtn5, '5');
+eventHandler(elBtn6, '6');
+eventHandler(elBtn7, '7');
+eventHandler(elBtn8, '8');
+eventHandler(elBtn9, '9');
